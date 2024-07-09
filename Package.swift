96ece4989a8c1c4e0d0924073862916d29fd9bf0
Philippe Weidmann <philippe.weidmann@infomaniak.com>
2024-07-09 13:46:55 +0200

    fix: Temporarily remove macOS target to allow Tuist generate

--- Package.swift
+++ Package.swift
@@ -4,7 +4,7 @@ import PackageDescription
 
 let package = Package(
     name: "mqtt-nio",
-    platforms: [.macOS(.v10_14), .iOS(.v12), .tvOS(.v12), .watchOS(.v6)],
+    platforms: [.iOS(.v12), .tvOS(.v12), .watchOS(.v6)],
     products: [
         .library(name: "MQTTNIO", targets: ["MQTTNIO"]),
     ],
@@ -27,7 +27,6 @@ let package = Package(
                 .product(name: "NIOConcurrencyHelpers", package: "swift-nio"),
                 .product(name: "NIOHTTP1", package: "swift-nio"),
                 .product(name: "NIOWebSocket", package: "swift-nio"),
-                .product(name: "NIOSSL", package: "swift-nio-ssl", condition: .when(platforms: [.linux, .macOS])),
                 .product(name: "NIOTransportServices", package: "swift-nio-transport-services"),
             ]
         ),
